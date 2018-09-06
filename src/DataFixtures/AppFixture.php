5558ac694d48101af488e85505cd26468abffc4d
jorobash <jorkomzx@gmail.com>
2018-09-06 14:56:48 +0300

    add new entity form

---
+++ src/DataFixtures/AppFixture.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+use App\Entity\MicroPost;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixture extends Fixture
+{
+    public function load(ObjectManager $manager){
+        for($i = 0; $i < 10; $i ++){
+            $microPost = new MicroPost();
+            $microPost->setText('Some random text ' . rand(0, 100));
+            $microPost->setTime(new \DateTime('2018-09-06'));
+            $manager->persist($microPost);
+        }
+
+        $manager->flush();
+    }
+}
